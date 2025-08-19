1f39d17d135ee5c38d7ddf51b95c7620919372c5
Michael Crothers <networkmikeandyou@gmail.com>
2025-08-19 15:20:50 -0700

    Add files via upload

--- Appinstaller.ps1
+++ Appinstaller.ps1
@@ -1,37 +1,14 @@
-# Install preferred programs. Still ongoing.
-# install-module Microsoft.WinGet.Client 
-# new-item -Path C:\ -Name Programlist.csv -ItemType File -Value 'steam, discord, chrome' 
-# need to differentiate between 32 bit and 64 bit OS 
-# need to make sure it installs the first one from the winget source not the msstore source
-<#
+# Installs applications by name. If the name doesn't narrow it down enough or there are multiple sources to download from 
+# it will open a web browser showing the different sources so you can pick and choose. 
+# You will need to open powershell as Administrator and Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 
-winget install discord.discord 
-winget install valve.steam 
-winget install RARLab.WinRAR 
-winget install google.chrome 
-winget install microsoft.visualstudiocode
-winget install github.githubdesktop
-winget install git.git
-winget install microsoft.powershell
-winget install insecure.nmap
-winget install adobe.acrobat.reader.64-bit
-winget install videolan.vlc
-steam, acrobatreader, discord, winrar, chrome, vscode, powershell, nmap, vlc
-https://winget.run/search?query=zoom
-#>
-Set-ExecutionPolicy -Scope CurrentUser -force -ExecutionPolicy RemoteSigned
-
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 $Createlist = read-host -Prompt 'Name of programs you wish to install.
 (Example: Chrome, Discord, Skype)' | Out-File -FilePath $env:TEMP\programlist.csv
-
 $Programs = Get-Content -Delimiter ', ' -Path $env:temp\programlist.csv
-
 foreach ($program in $programs) {
-    if ((winget install --moniker $program.TrimEnd() --no-upgrade) -match 'Multiple packages found matching input criteria. Please refine the input.')
-    {Start-Process https://winget.run/search?query=$program}
-    else {winget install --moniker $program.TrimEnd() --no-upgrade}
+    if ((winget install --moniker $program.TrimEnd(', ')) -match 'Multiple packages found matching input criteria. Please refine the input.')
+    { Start-Process https://winget.run/search?query=$program }
+    else { winget install --moniker $program.TrimEnd(', ') }
 }
-
 remove-item -Path $env:temp\programlist.csv
-
-
