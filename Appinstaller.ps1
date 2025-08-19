586ad61518d44430d3164d386cfac15a0b0af8c8
Michael Crothers <networkmikeandyou@gmail.com>
2025-08-19 15:23:59 -0700

    Add files via upload

--- Appinstaller.ps1
+++ Appinstaller.ps1
@@ -1,7 +1,7 @@
 # Installs applications by name. If the name doesn't narrow it down enough or there are multiple sources to download from 
 # it will open a web browser showing the different sources so you can pick and choose. 
 # Cheers!
-
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
 $Createlist = read-host -Prompt 'Name of programs you wish to install.
 (Example: Chrome, Discord, Skype)' | Out-File -FilePath $env:TEMP\programlist.csv
 $Programs = Get-Content -Delimiter ', ' -Path $env:temp\programlist.csv
