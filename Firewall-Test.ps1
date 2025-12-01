6183f181e7a4e81c03e78d51ec7d05b21bb22c9f
Michael Crothers <networkmikeandyou@gmail.com>
2025-12-01 15:40:57 -0800

    New Scripts

--- Firewall-Test.ps1
+++ Firewall-Test.ps1
@@ -1,8 +1,7 @@
-param (
-        [parameter(
-        Mandatory = $false)]
-        [string]$result
-    )
-    $result = (get-netfirewallProfile -PolicyStore ActiveStore | Select-Object -Property Name, Enabled)
+$firewall = (get-netfirewallProfile -PolicyStore ActiveStore | Select-Object -Property Enabled)
 
-    if ($result.Enabled -eq $True){write-host 'this is true'}
+if ($firewall -notcontains $false) {
+    $true
+} else {
+    $false
+}
